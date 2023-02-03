9112dd0e5eca67bbdb86fbdc3aa7588ce247d056
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-02-03 17:21:36 +0100

    AcerOne8T482L: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: acer Model: Acer One 8 T4-82L Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Acer Acer One 8 T4-82L (AcerOne8T482L)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_AcerOne8T482L-user
+add_lunch_combo omni_AcerOne8T482L-userdebug
+add_lunch_combo omni_AcerOne8T482L-eng
